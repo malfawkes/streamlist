5ea9f767ff5fa6272c4fbc6414ba8641b7164297
marlou <marlou.fawkes@gmail.com>
2026-08-26 19:25:26 +0800

    Added relevant folders and files

---
+++ config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'streamlist');
+define('DB_USER', 'your_db_username');
+define('DB_PASS', 'your_db_password');
+
+define('TMDB_API_KEY', 'your_tmdb_api_key');
+
+define('SITE_URL', 'http://localhost/streamlist');
+define('FREE_TIER_WATCHLIST_LIMIT', 20);
